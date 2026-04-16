c4cd3fe07af186483d325e809c3831685c69af5a
luisg <luisgardaz13@gmail.com>
2026-04-16 12:38:38 -0500

    chore: carpeta que tenemos que crear manualmente

--- lab-01/apache2-foreground.sh
+++ lab-01/apache2-foreground.sh
@@ -4,6 +4,9 @@ echo " Contenedor corriendo en puerto 80"
 # Para que Apache encuentre sus propias variables
 source /etc/apache2/envvars
 
+# Crear la carpeta que apache necesita
+mkdir -p /var/run/apache2
+
 # Para que sea el nuevo proceso principal
 # -D, quiere decir define esta variable
 # FOREGROUND, para que no se vaya al background
